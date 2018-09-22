32a698b365e7c65304370871377069e46482df12
Mahbub Alam Evan <mahbubalamevan@Mahbubs-MacBook-Air.local>
2018-09-22 11:34:23 +0600

    JavaScript

---
+++ object.js
@@ -0,0 +1,13 @@
+var person = {
+            name:"Evan",
+            age:25,
+            gender:"male",
+}
+
+console.log(person.name);
+//document.write(person.age);
+
+f1 = function(){
+
+  alert ("Mahbub"+" "+"Evan");
+}
---
+++ test.js
@@ -0,0 +1,6 @@
+//var x = 50;
+//document.write(x);
+"use strict"
+var x = 50;
+//X = 300;
+//document.write(x);
